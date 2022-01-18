adf783c6582ac58e69f1f81086ec0f255e7ce2d9
ChuanyiZ <chuanyi5@illinois.edu>
2022-01-18 16:55:17 -0600

    version 0.1.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     description="Core Sequence Identifier",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version='0.1.1',
+    version='0.1.2',
     url='http://github.com/elkebir-group/CORSID',
     author='Chuanyi Zhang',
     author_email='chuanyi5@illinois.edu',
